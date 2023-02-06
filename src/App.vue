cc157acb76c6afff34d74c2f38d1789225fd1c29
paweld139 <pawell139139@gmail.com>
2023-02-06 21:14:08 +0100

    Dev

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <OpenLayersWmts />
+    <OpenLayersDemo />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    import HelloWorld from './components/HelloWorld.vue';
+    //import OpenLayersDemo from './components/OpenLayersDemo.vue';
+    //import OpenLayersWmts from './components/OpenLayersWmts.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default defineComponent({
+        name: 'App',
+        components: {
+            HelloWorld//,
+            //OpenLayersDemo,
+            //OpenLayersWmts
+        }
+    });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
